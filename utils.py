8848ce5a349dddeb608a18f526e0f6cbf112f176
alex <karuk1998@yandex.ru>
2020-07-19 08:20:35 +0300

    improved convert function

--- utils.py
+++ utils.py
@@ -4,17 +4,17 @@ import cv2
 import os
 
 
-def convert(file, dpi=300, image_format='jpeg', color_mode='rgb'):
+def convert(file, dpi=300, image_format='jpg', color_mode='rgb'):
     """
     Convert pdf file to selected format
 
     Args:
-        file (str or bytes):
-            File or path to PDF file that need to be converted
+        file (str):
+            Path to PDF file that need to be converted
         dpi (int):
             DPI of converter
         image_format (str):
-            format of output file. Possible formats: ['jpeg', 'png']
+            format of output file. Possible formats: ['jpg', 'png']
         color_mode (str):
             Color mode of output images
             Possible modes: ['rgb', 'rgba', 'grayscale', 'binary']
@@ -24,22 +24,30 @@ def convert(file, dpi=300, image_format='jpeg', color_mode='rgb'):
     transparent = color_mode == 'rgba'
     grayscale = color_mode == 'grayscale'
 
-    if type(file) == str:
-        with open(file, 'rb') as f:
-            file = f.read()
-    converted = convert_from_bytes(file, dpi, fmt=image_format,
+    with open(file, 'rb') as f:
+        file = f.read()
+    converted = convert_from_bytes(file, dpi, fmt='ppm',
                                    transparent=transparent, grayscale=grayscale)
-    converted = [np.array(im) for im in converted]
-    if color_mode == 'binary':
-        converted = [cv2.threshold(
-            cv2.cvtColor(im, cv2.COLOR_RGB2GRAY), 128,
-            255, cv2.THRESH_BINARY)[1] for im in converted]
-    # elif color_mode == 'rgb':
-    #     converted = [cv2.cvtColor(im, cv2.COLOR_BGR2RGB) for im in converted]
-    # elif color_mode == 'rgba':
-    #     converted = [cv2.cvtColor(im, cv2.COLOR_BGRA2RGBA) for im in converted]
-    # elif color_mode == 'grayscale':
-    #     converted = [cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) for im in converted]
+    if color_mode == 'rgb':
+        converted = [cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
+                     for im in converted]
+    elif color_mode == 'rgba':
+        if image_format != 'jpg':
+            converted = [cv2.cvtColor(np.array(im), cv2.COLOR_RGBA2BGRA)
+                         for im in converted]
+        else:
+            converted = [cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
+                         for im in converted]
+    elif color_mode == 'binary':
+        converted = [cv2.cvtColor(cv2.threshold(
+            cv2.cvtColor(np.array(im), cv2.COLOR_RGB2GRAY), 128,
+            255, cv2.THRESH_BINARY)[1], cv2.COLOR_GRAY2BGR) for im in converted]
+    else:
+        # Grayscale
+        converted = [
+            cv2.cvtColor(cv2.cvtColor(np.array(im), cv2.COLOR_RGB2GRAY),
+                         cv2.COLOR_GRAY2BGR)
+            for im in converted]
     return converted
 
 
