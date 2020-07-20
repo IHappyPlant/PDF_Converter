f2b6de96b39dbfc93c881bdee13930dbb027258f
Александр Карюк <43047239+ihappyplant@users.noreply.github.com>
2020-07-21 00:40:43 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+required_packages = [
+    'pyqt5',
+    'pdf2image',
+    'numpy',
+    'opencv-Python'
+]
+
+setup(
+    name='pdf_converter',
+    version='1.0',
+    url='https://github.com/IHappyPlant/Pdf_Converter',
+    author='IHappyPlant',
+    author_email='karuk1998@yandex.ru',
+    license='GPLv3',
+    description='GUI app to converting files from PDF to image formats',
+    packages=["converter"],
+    install_requires=required_packages,
+    include_package_data=True,
+    python_requires='>=3.6',
+    keywords="pdf jpg png converter image format gui application app",
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Utilities',
+        'Operating System :: POSIX :: Linux',
+        'Natural Language :: English',
+        'Environment :: X11 Applications :: Qt',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Education'
+    ]
+)
