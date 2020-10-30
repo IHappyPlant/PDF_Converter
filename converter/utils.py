db044c6375bd9ceb1c532e755b4de1dac8a7d293
IHappyPlant <karuk1998@yandex.ru>
2020-10-30 22:45:09 +0300

    * added comments and docstrings * improved get_file_name function

--- converter/utils.py
+++ converter/utils.py
@@ -1,4 +1,8 @@
-from os.path import basename
+# coding=utf-8
+"""
+This module contains utility functions for pdf converter
+"""
+from os.path import basename, splitext
 
 import cv2
 from numpy import array
@@ -27,8 +31,10 @@ def convert(file, dpi=300, image_format='jpg', color_mode='rgb'):
 
     with open(file, 'rb') as f:
         file = f.read()
+    # Convert document to Pillow images
     converted = convert_from_bytes(file, dpi, fmt=image_format,
                                    transparent=transparent, grayscale=grayscale)
+    # Convert colors from RGB(A) to BGR(A)
     if color_mode == 'rgb':
         converted = [cv2.cvtColor(array(im), cv2.COLOR_RGB2BGR)
                      for im in converted]
@@ -40,15 +46,24 @@ def convert(file, dpi=300, image_format='jpg', color_mode='rgb'):
             converted = [cv2.cvtColor(array(im), cv2.COLOR_RGB2BGR)
                          for im in converted]
     elif color_mode == 'binary':
+        # Apply binary threshold to image
         converted = [cv2.threshold(
             cv2.cvtColor(array(im), cv2.COLOR_RGB2GRAY), 128, 255,
             cv2.THRESH_BINARY)[1] for im in converted]
     else:
-        # Grayscale
+        # Convert RGB to grayscale
         converted = [cv2.cvtColor(array(im), cv2.COLOR_RGB2GRAY)
                      for im in converted]
     return converted
 
 
 def get_file_name(file_path):
-    return basename(file_path).split('.')[0]
+    """
+    Get name of the file
+
+    Args:
+        file_path (str): Path to file
+    Returns:
+        (str): Name of file without extension
+    """
+    return splitext(basename(file_path))[0]
