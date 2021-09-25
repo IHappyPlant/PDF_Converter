da5122a119cad71e70377b0a5b6012be7d2b4490
IHappyPlant <karuk1998@yandex.ru>
2021-09-25 11:01:21 +0300

    removed 'binary' color mode and cleaned up convert function code

--- converter/utils.py
+++ converter/utils.py
@@ -15,7 +15,7 @@ def convert(file, dpi=300, image_format='jpg', color_mode='rgb'):
     :param str image_format: format of output file.
         Possible formats: ['jpg', 'png']
     :param str color_mode: Color mode of output images.
-        Possible modes: ['rgb', 'rgba', 'grayscale', 'binary']
+        Possible modes: ['rgb', 'rgba', 'grayscale']
     :return: list of converted pages
     :rtype: list[numpy.ndarray]
     """
@@ -28,20 +28,6 @@ def convert(file, dpi=300, image_format='jpg', color_mode='rgb'):
     converted = convert_from_path(file, dpi, fmt=image_format,
                                   transparent=transparent, grayscale=grayscale,
                                   thread_count=cpu_count)
-
-    # TODO: add multiprocessing to this
-    if color_mode == 'rgb':
-        converted = [im.convert('RGB') for im in converted]
-    elif color_mode == 'rgba':
-        if image_format == 'png':
-            converted = [im.convert('RGBA') for im in converted]
-        else:
-            converted = [im.convert('RGB') for im in converted]
-    elif color_mode == 'binary':
-        converted = [im.convert('1') for im in converted]
-    else:
-        # Convert to grayscale
-        converted = [im.convert('L') for im in converted]
 
     return [array(im) for im in converted]
 
