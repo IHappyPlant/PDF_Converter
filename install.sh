ed90136581f38c0239977ddaf5d393a042ce0af0
IHappyPlant <ihappyplant@gmail.com>
2022-01-10 01:05:37 +0300

    minor fix

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 # Create binary file using pyinstaller
 venv_name=/tmp/temp_venv_$RANDOM
 executable_name=pdf_converter
+pip3 install virtualenv
 python3 -m venv $venv_name
 source $venv_name/bin/activate
 pip install --no-cache-dir pyinstaller pyqt5 pdf2image numpy pillow
