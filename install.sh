8e6e85ff22c96f81c6a880e5e929f9859ba97bf0
IHappyPlant <karuk1998@yandex.ru>
2022-01-09 22:51:45 +0300

    added installation script for Linux

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Create binary file using pyinstaller
+venv_name=/tmp/temp_venv_$RANDOM
+executable_name=pdf_converter
+python3 -m venv $venv_name
+source $venv_name/bin/activate
+pip install --no-cache-dir pyinstaller pyqt5 pdf2image numpy pillow
+pyinstaller converter/__main__.py --onefile --clean -n$executable_name
+deactivate
+
+rm $executable_name.spec
+rm -rf build $venv_name
