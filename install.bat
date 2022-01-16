be4fd7283672a941a92d891580957c4172d26880
IHappyPlant <karuk1998@yandex.ru>
2022-01-16 19:52:05 +0300

    minor installation script changes

--- install.bat
+++ install.bat
@@ -4,8 +4,9 @@ pip3 install virtualenv
 python3 -m venv %venv_name%
 
 .\%venv_name%\Scripts\activate.bat & ^
-pip install pyinstaller pyqt5 pdf2image numpy pillow & ^
-pyinstaller converter\__main__.py --windowed --clean -n%executable_name% & ^
+pip install --no-cache-dir pyinstaller pyqt5 pdf2image numpy pillow & ^
+pyinstaller converter\__main__.py --windowed --onefile ^
+--clean -n%executable_name% & ^
 del %executable_name%.spec & ^
 rmdir /S /Q build & ^
 rmdir /S /Q %venv_name%
